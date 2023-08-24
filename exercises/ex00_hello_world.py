0cea300ef2aede15698a16a00fd4d89e6111a3d7
alexperez21 <alexperez6104@gmail.com>
2023-08-24 10:26:50 -0400

    Finished Exercise 00!

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,5 +1,6 @@
 """My first program for COMP110."""
 
-__author__ ="730672001"
+__author__ = "730672001"
 
-print("Hello welcome to the wonderful world.")
+
+print("Hello welcome to the wonderful world.")   
